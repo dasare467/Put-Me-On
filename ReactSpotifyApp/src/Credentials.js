02f7919c4a790754203a8f142df6b483e6c4ee90
dasare467 <112352313+dasare467@users.noreply.github.com>
2023-01-17 20:53:32 -0500

    Update Credentials

--- ReactSpotifyApp/src/Credentials.js
+++ ReactSpotifyApp/src/Credentials.js
@@ -1,8 +1,8 @@
 const Credentials = () => {
     
     return {
-        ClientId: 'bbdba9d33a94452d99c8ad6265848131',
-        ClientSecret: '45481fed0ca64752b897bc8b05fd9519'
+        ClientId: 'ENTER CLIENT ID',
+        ClientSecret: 'ENTER CLIENT SECRET'
     }
 }
 
